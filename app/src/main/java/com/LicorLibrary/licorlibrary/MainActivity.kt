0b4f844567f3678da652c5b2631c6de0d38a23de
ManueloRZ2 <nuelarbelaez@gmail.com>
2022-01-26 20:41:35 -0500

    viewbinding para reemplazar el referenciado a los objetos

--- app/src/main/java/com/LicorLibrary/licorlibrary/MainActivity.kt
+++ app/src/main/java/com/LicorLibrary/licorlibrary/MainActivity.kt
@@ -5,24 +5,30 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.LicorLibrary.licorlibrary.databinding.ActivityMainBinding
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
 
-        val TextViewName : EditText = findViewById(R.id.EditTextName)
+        /*val TextViewName : EditText = findViewById(R.id.EditTextName)
         val TextViewAuthor : EditText = findViewById(R.id.TextViewAuthor)
         val TextInputCapacity : EditText = findViewById(R.id.TextInputCapacity)
 
         val SaveButton : Button = findViewById(R.id.ButtonSave)
-        val SaveTextView : TextView = findViewById(R.id.TextViewButton)
+        val SaveTextView : TextView = findViewById(R.id.TextViewButton)*/
 
-        SaveButton.setOnClickListener{
-            val Information : String = TextViewName.text.toString() + ", " + TextViewAuthor.text.toString() + ", " + TextInputCapacity.text.toString()
 
-            SaveTextView.text = Information
+        mainBinding.ButtonSave.setOnClickListener{
+            val Information : String = mainBinding.textViewName.text.toString() + ", " + mainBinding.TextViewAuthor.text.toString() + ", " + mainBinding.TextInputCapacity.text.toString()
+
+            mainBinding.TextViewButton.text = Information
         }
     }
 }
