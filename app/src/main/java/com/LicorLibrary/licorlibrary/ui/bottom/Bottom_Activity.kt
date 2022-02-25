fba1c576c40d5e1d33ba7ee53889ffb1cf05d600
ManueloRZ2 <nuelarbelaez@gmail.com>
2022-02-24 21:25:55 -0500

    buttom navigation

---
+++ app/src/main/java/com/LicorLibrary/licorlibrary/ui/bottom/Bottom_Activity.kt
@@ -0,0 +1,36 @@
+package com.LicorLibrary.licorlibrary.ui.bottom
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.LicorLibrary.licorlibrary.R
+import com.LicorLibrary.licorlibrary.databinding.BottomActivityBinding
+
+class   Bottom_Activity : AppCompatActivity() {
+
+    private lateinit var binding: BottomActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = BottomActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_bottom_activity)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/LicorLibrary/licorlibrary/ui/update/UpdateViewModel.kt
@@ -0,0 +1,7 @@
+package com.LicorLibrary.licorlibrary.ui.update
+
+import androidx.lifecycle.ViewModel
+
+class UpdateViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
